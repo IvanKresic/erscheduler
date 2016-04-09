3366b566b4ea6fde87b0d494d0e41adefea179e9
majabio <maja23miljak@gmail.com>
2016-04-09 15:35:35 +0200

    klasa employees dodajem

---
+++ erscheduler/Models/Employees.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace erscheduler.Models
+{
+    public class Employees
+    {
+        public int ID { get; set; }
+        public string Employment { get; set; }
+        public int OIB { get; set; }
+        public int Priority { get; set; }
+        public bool Status { get; set; }
+    }
+}
