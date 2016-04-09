b60e1352d2df3903e608fa6efee6f6b7edf07b01
Ivan Krešić <ik017703661@fer.hr>
2016-04-09 13:20:13 +0200

    Second_Commit

---
+++ erscheduler/Models/Gender.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace erscheduler.Models
+{
+    public class Gender
+    {
+        public int Gender_ID { get; set; }
+        public string Gender_Type { get; set; }
+    }
+}
