7dd83826c93001c643a23506d3161390cdd98fad
Ivan Krešić <ik017703661@fer.hr>
2016-04-09 13:32:10 +0200

    Commit suicide

---
+++ erscheduler/Models/Work_Data.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace erscheduler.Models
+{
+    public class Work_Data
+    {
+        public int Worker_ID { get; set; }
+        public DateTime Shift_Date_End { get; set; }
+        public DateTime Shift_Time_End { get; set; }
+        public int Working_Hours { get; set; }
+
+    }
+}
